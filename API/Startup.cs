38094aaaa2a54be10d88d5ac115fb1b75037e6e9
ShirBoxer <shirboxer@gmail.com>
2021-08-03 22:05:48 +0300

    bug fixed

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,10 +41,10 @@ namespace API
             services.AddCors();
             services.AddIdentityServices(_config);
 
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            // });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,8 +55,8 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
                 //IdentityModelEventSource.ShowPII = true; 
             }
 
@@ -68,6 +68,7 @@ namespace API
             .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
