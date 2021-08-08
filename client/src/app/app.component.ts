a2cbcf458f0618deff1fd6e498bb56bcc562eb6f
ShirBoxer <shirboxer@gmail.com>
2021-08-08 22:53:07 +0300

    home and main components style

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
     title = 'Dogs Salon';
